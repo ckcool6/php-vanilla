996bcfe93e4f6bc7769ae0830bdeb9f18451ba39
ckcool6 <rzdtjb@gmail.com>
2025-01-05 19:28:01 +0800

    composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,11 +3,11 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require 'core/Router.php';
+/* require 'core/Router.php';
 require 'core/Request.php';
 require 'core/database/Connection.php';
 require 'core/database/QueryBuild.php';
-
+ */
 
 
 $app['database'] = new QueryBuild(
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '50066ddc9a5daef82f8916579b3dbc48707fea1c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '50066ddc9a5daef82f8916579b3dbc48707fea1c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
